958355ba84d0239e5346a56977ba4835b66d7193
Alex Werthmann <werdy@gmx.net>
2016-01-25 01:01:08 +0100

    min change

--- src/learn/conjugation/MainMenuState.java
+++ src/learn/conjugation/MainMenuState.java
@@ -17,11 +17,14 @@ public class MainMenuState implements State {
 	 * 
 	 */
 	@Override
-	public void execute(BufferedReader in, BufferedWriter out) throws IOException {
+	public void execute(BufferedReader in, BufferedWriter out)
+			throws IOException {
 		boolean fail = false;
-		out.write("Konjugations-Trainer Hauptmenü\n" + "1: Lernbox erstellen\n" + "2: Lernbox laden\n"
-				+ "3: Wort suchen\n" + "4: Gesamtes Wörterbuch lernen\n" + "5: Wörterbuch speichern\n"
-				+ "q: Programm beenden\n" + "Bitte Option(Zahl) eingeben:");
+		out.write("Konjugations-Trainer Hauptmenü\n" + "1: Lernbox erstellen\n"
+				+ "2: Lernbox laden\n" + "3: Wort suchen\n"
+				+ "4: Gesamtes Wörterbuch lernen\n"
+				+ "5: Wörterbuch speichern\n" + "q: Programm beenden\n"
+				+ "Bitte Option(Zahl) eingeben:");
 		out.newLine();
 		out.flush();
 		String input = in.readLine();
@@ -37,15 +40,18 @@ public class MainMenuState implements State {
 			if (!fail)
 				switch (zahl) {
 				case 1: {
-					ConjugationTrainer.changeState(ConjugationTrainer.createBoxState);
+					ConjugationTrainer
+							.changeState(ConjugationTrainer.createBoxState);
 					break;
 				}
 				case 2: {
-					ConjugationTrainer.changeState(ConjugationTrainer.loadBoxState);
+					ConjugationTrainer
+							.changeState(ConjugationTrainer.loadBoxState);
 					break;
 				}
 				case 3: {
-					ConjugationTrainer.changeState(ConjugationTrainer.searchWordState);
+					ConjugationTrainer
+							.changeState(ConjugationTrainer.searchWordState);
 					break;
 				}
 				case 4: {
